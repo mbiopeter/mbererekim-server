1d7fc6eabfa7cfd50302c9eaa8869fb88942f0eb
mbio Peter <mbiopeter401@gmail.com>
2024-12-17 15:07:44 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 // /config/database.js
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'electronicquotation',
+    'mbererekim',
+    'austropithecus!',
     {
-        host: process.env.DB_HOST,
+        host: 'mbererekim.cpko42qeaxdt.eu-north-1.rds.amazonaws.com',
         dialect: 'mysql',
+        port: 3306, 
     }
 );
 
